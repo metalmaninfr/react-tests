da7d36aceae2f84cdd80587ab3d77d5cd37f595a
Blandford Alex <alex.blandford@hetic.net>
2020-04-23 14:48:24 +0200

    added storybook deploy

---
+++ storybook-static/precache-manifest.d2c3f616001b3806494a9e77308abdbe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a0e56c4bc365d6a3c3fee6251306eb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b1578e91824af05737d5.bundle.js"
+  },
+  {
+    "url": "runtime~main.b1578e91824af05737d5.bundle.js"
+  },
+  {
+    "url": "vendors~main.b1578e91824af05737d5.bundle.js"
+  },
+  {
+    "url": "vendors~main.b1578e91824af05737d5.bundle.js.LICENSE.txt"
+  }
+]);
