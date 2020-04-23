dfdb2026f7e5a5dcad6c19eb7b7b25e13ffbb895
Blandford Alex <alex.blandford@hetic.net>
2020-04-23 10:19:25 +0200

    fixed test

--- src/features/link.test.js
+++ src/features/link.test.js
@@ -4,13 +4,13 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
 
-import { NavLink } from '../stories/link.stories';
+import { Link } from '../stories/link.stories';
 
 describe('Renders NavLink component', () => {
     test('with default behavior', () => {
-      const { container, getByTestId } = render(<NavLink />);
+      const { container, getByTestId } = render(<Link />);
+      expect(container).toBeTruthy();
       const link = getByTestId('link')
-      link.click();
-      expect(container.className).toContain("active");
+      expect(link).toHaveStyleRule('text-decoration', 'none');
     });
   });
